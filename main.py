84928abc7df73d1381adfd8480f1a778abe418a3
Andrey <tryastsinandrei@yandex.ru>
2023-07-07 11:20:40 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# from app import app
-# from app import db
-from view import *
+from app import app
+from app import db
+import view
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
