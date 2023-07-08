0dfb1d0c81f37a983adc9f2529657c0e769a1803
Andrey <tryastsinandrei@yandex.ru>
2023-07-08 11:22:18 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#from app import app
-#from app import db
-#import view
-from view import *
+from app import app
+from app import db
+import view
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0')
